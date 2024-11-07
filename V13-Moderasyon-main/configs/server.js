9827a03c4c7861b3ab662fe4e8a4c8c7db620311
Aslanxbx <neclaaslan14222@gmail.com>
2024-11-07 21:52:33 +0300

    Add files via upload

---
+++ V13-Moderasyon-main/configs/server.js
@@ -0,0 +1,14 @@
+module.exports = {
+    embedColor: "RANDOM",
+    admins: ["1194307708824850605"], //! BOT OWNER ID
+    serverId: "1271076386966339616", //! BOT YAPTIĞINIZ SUNUCU ID
+    token: "MTMwMzA2ODE2OTIwNzkzOTE3Mw.GpA2R7.wwiAfCJT6cQIfPc0UmfNcYIbD0dDXm-8IbvSoA", //! BOT TOKENİ
+    mongoURL: "mongodb+srv://passbots:passbots@cluster0.glaiytc.mongodb.net/canismuwsic?retryWrites=true&w=majority", //! MONGO DB LİNKİ https://www.mongodb.com/
+    status: ["Anıtkabir💝Pıcharon"], //! BOT OYNUYOR KISMI
+    muteLimit: 10,
+    banLimit: 5,
+    vmuteLimit: 10,
+    jailLimit: 6,
+    tag: "∞", //! TAGINIZ
+    tagsıztag: "•",
+}
